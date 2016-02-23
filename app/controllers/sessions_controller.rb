b21dce743cfa6df6222c3acb68ad145bdd0e7a82
Yana Kultysheva <ykultysheva@gmail.com>
2016-02-23 16:24:35 -0500

    Links "home", "new project", "login & sing up" are added to layout file.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to projects_url, notice "Logged out"
+    redirect_to projects_url, notice: "Logged out!"
   end
 end
