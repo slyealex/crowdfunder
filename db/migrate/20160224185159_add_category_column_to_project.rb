2bc0096f9ad5a5b33cdb85390b5968c474348b06
ALEX SLYE <slyealex@live.com>
2016-02-24 14:18:58 -0500

    add category to project model

---
+++ db/migrate/20160224185159_add_category_column_to_project.rb
@@ -0,0 +1,5 @@
+class AddCategoryColumnToProject < ActiveRecord::Migration
+  def change
+    add_column :projects, :category, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224041357) do
+ActiveRecord::Schema.define(version: 20160224185159) do
 
   create_table "pledges", force: :cascade do |t|
     t.integer  "amount"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160224041357) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "category"
   end
 
   add_index "projects", ["user_id"], name: "index_projects_on_user_id"
