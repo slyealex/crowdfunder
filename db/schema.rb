1e450465eb55ae55b12a25e628ecc1bc07ebdf98
ALEX SLYE <slyealex@live.com>
2016-02-25 16:41:18 -0500

    add comment model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224185159) do
+ActiveRecord::Schema.define(version: 20160225213822) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "comment"
+    t.integer  "user_id"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["project_id"], name: "index_comments_on_project_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "pledges", force: :cascade do |t|
     t.integer  "amount"
